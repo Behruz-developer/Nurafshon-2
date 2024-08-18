a577818e958d7f95b9f89ebc25522dbe881af831
Behruz-developer <88540036+Behruz-developer@users.noreply.github.com>
2024-08-18 19:50:35 +0500

    update

--- src/Components/Construction/Construction.jsx
+++ src/Components/Construction/Construction.jsx
@@ -1,23 +1,34 @@
+import { useEffect } from 'react'
 import construction from '../../assets/images/construction.png'
 import construction2 from '../../assets/images/construction2.png'
 import construction3 from '../../assets/images/construction3.png'
 import construction4 from '../../assets/images/construction4.png'
 import AOS from 'aos'
 import 'aos/dist/aos.css';
-import { useEffect } from "react";
 
 const Construction = () => {
     useEffect(() => {
-        AOS.init({
-            duration: 1000,
-            once: true,
-        });
+        const initAOS = () => {
+            AOS.init({
+                duration: 1000,
+                once: true,
+                offset: window.innerWidth < 1400 ? 200 : 400, 
+            });
+        };
+
+        initAOS();
+        window.addEventListener('resize', initAOS);
+
+        return () => {
+            window.removeEventListener('resize', initAOS);
+        };
     }, []);
+    
     return (
         <div className="construction">
             <div className="container">
                 <h2 className="construction_title">Qurilish jarayoni</h2>
-                <div className="construction_box" data-aos="zoom-in-up" data-aos-offset="400">
+                <div className="construction_box" data-aos="zoom-in-up">
                     <div className="construction_card">
                         <img src={construction} alt="" />
                         <p className="construction_text">Iyun</p>
