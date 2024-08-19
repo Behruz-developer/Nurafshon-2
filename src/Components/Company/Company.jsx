5bc3179c3ab5f9da1e9152da97d2554cc6241976
Behruz-developer <88540036+Behruz-developer@users.noreply.github.com>
2024-08-19 21:05:45 +0500

    update

--- src/Components/Company/Company.jsx
+++ src/Components/Company/Company.jsx
@@ -14,44 +14,44 @@ const Company = () => {
   const sectionRef = useRef(null);
 
   useEffect(() => {
-      const handleScroll = () => {
-          if (!hasAnimated && sectionRef.current) {
-              const sectionTop = sectionRef.current.getBoundingClientRect().top;
-              const windowHeight = window.innerHeight;
+    const handleScroll = () => {
+      if (!hasAnimated && sectionRef.current) {
+        const sectionTop = sectionRef.current.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
 
-              if (sectionTop < windowHeight) {
-                  animateNumber(0, 1684, 1000, setNumber1);
-                  animateNumber(0, 188430, 1000, setNumber2);
-                  setHasAnimated(true); 
-              }
-          }
-      };
+        if (sectionTop < windowHeight) {
+          animateNumber(0, 1684, 1000, setNumber1);
+          animateNumber(0, 188430, 1000, setNumber2);
+          setHasAnimated(true);
+        }
+      }
+    };
 
-      window.addEventListener('scroll', handleScroll);
+    window.addEventListener('scroll', handleScroll);
 
-      return () => {
-          window.removeEventListener('scroll', handleScroll);
-      };
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
   }, [hasAnimated]);
   const animateNumber = (start, end, duration, setNumber) => {
     const increment = (end - start) / (duration / 1);
     let currentNumber = start;
 
     const timer = setInterval(() => {
-        currentNumber += increment;
-        if (currentNumber >= end) {
-            setNumber(end);
-            clearInterval(timer);
-        } else {
-            setNumber(Math.round(currentNumber));
-        }
+      currentNumber += increment;
+      if (currentNumber >= end) {
+        setNumber(end);
+        clearInterval(timer);
+      } else {
+        setNumber(Math.round(currentNumber));
+      }
     }, 1);
-};
+  };
   return (
     <div className="company" >
       <div className="container">
         <div className="company_box">
-        <div className="company_card_img">
+          <div className="company_card_img">
             <img src={universal} alt="" className="company_img" />
 
           </div>
@@ -59,38 +59,18 @@ const Company = () => {
             <div className="company_card_top">
               <h2 className="company_title">Quruvchi kompaniya to‘g‘risida</h2>
               <p className="company_text">
-                Sizning uyingizda hozirdan baxtli oilalar yashamogdalar,
-                bolajonlar birinchi do'stlarini topmoqdalar va do stona
-                qo'shnichilik madaniyati gullab-yashnamoqda.
+                TXT Group yirik va zamonaviy qurilish kompaniyasi boʻlib, Oʻzbekistonning 10 dan ortiq tuman va shaharlarida turar-joy majmualari barpo etgan. Kompaniya 2014-yilda tashkil etilgan va shu vaqt ichida ishonchli hamda samarali hamkor sifatida tanilgan.
               </p>
               <p className="company_text">
-                NRG - O'zbekiston va Qozog'iston qardosh xalqlarining do'stligi
-                va madaniy birligining, hamfikrlilik ittifogining yaggol
-                tasdig'idir. Biz o'zbek xalgining ko'p asrlik eng zo'r me'moriy
-                ananalari bilan Qozog'istonning ilg'or qurilish tajribasini
-                mujassamlashtirmoqdamiz.
+                Har bir loyiha biz uchun nafaqat biznes, balki oʻz mijozlarimizga eng yaxshi yashash sharoitlarini taqdim etish imkoniyatidir. Bizning asosiy missiyamiz mustahkam va qulay uy-joylar qurish orqali xalqimizning farovonligini taʼminlashdir.
               </p>
               <p className="company_text">
-                Biz hayot uchun qulay makon barpo etmoqdamiz, 24/7 sifatli
-                servis va maishiy masalalarda kafolatlangan yordam taqdim etamiz
+                Kompaniyamiz sifat, mustahkamlik va zamonaviy yondashuvlar asosida eng yuqori darajadagi qurilish xizmatlarini taqdim etishni oʻz oldiga maqsad qilgan. Har bir loyiha biz uchun oʻziga xosdir, shuning uchun biz nafaqat texnik koʻrsatkichlarga, balki mijozlarimizning istak va ehtiyojlariga ham katta eʼtibor qaratamiz.
               </p>
+              <p className="company_text">
+                Biz bilan hamkorlik qilib, siz barqaror va mustahkam qurilish yechimlarini tanlaysiz. Biz sizning ishonchingizni oqlash va orzularingizdagi loyihani hayotga tatbiq etish uchun doim harakatdamiz!              </p>
             </div>
-            <div className="company_card_bottom" ref={sectionRef}>
-              <div className="company_card_bottom_numbers">
-                <h3> + {number1.toLocaleString()}</h3>
-                <p>
-                  Oila bizning uylarda <br /> yashamoqda
-                </p>
-              </div>
-              <div className="company_card_bottom_numbers">
-                <h3>
-                + {number2.toLocaleString()} m<span>2</span>
-                </h3>
-                <p>
-                  Xonadonlar qurib bitkazilib <br /> o‘z egalariga topshirildi
-                </p>
-              </div>
-            </div>
+
           </div>
 
         </div>
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
       <div className="footer_box">
         <div className="footer_card2">
           <div className="map_box" >
-          <iframe className="map_container" src="https://yandex.com/map-widget/v1/?um=constructor%3Ae514b2e6ac1c9732d57ab79a10d83085ef358bf42ce31a6a5b17447548674b9e&amp;source=constructor" width="536" height="463" frameborder="0"></iframe>
+            <iframe className="map_container" src="https://yandex.com/map-widget/v1/?um=constructor%3Ae514b2e6ac1c9732d57ab79a10d83085ef358bf42ce31a6a5b17447548674b9e&amp;source=constructor" width="536" height="463" frameborder="0"></iframe>
           </div>
         </div>
         <div className="footer_card1">
@@ -27,7 +27,7 @@ const Footer = () => {
                 Ish vaqti
               </div>
               <p className="footer_location_text">
-                Har kun soat 09:00 dan 18:00 gacha
+                Har kun <br /> soat 09:00 dan 18:00 gacha
               </p>
             </div>
             <div className="footer_location">
@@ -37,8 +37,11 @@ const Footer = () => {
                 Sotuv bo‘limi
               </div>
               <p className="footer_location_text">
-                Toshkent shahri, Mirobod tumani, <br /> Oybek ko‘chasi 38а,
-                Biznes center "Avalon", 1 qavat
+                Samarqand viloyati tumani Kamangaron
+                <br />
+                koʻchasi 14-uy
+                <br />
+                Moʻljal: Urgut City
               </p>
             </div>
             <div className="footer_social">
@@ -50,7 +53,7 @@ const Footer = () => {
                 </div>
               </div>
               <div className="footer_icons">
-              <a href="https://www.instagram.com/txtgroup.uzb" className="footer_icon">
+                <a href="https://www.instagram.com/txtgroup.uzb" className="footer_icon">
                   <FaInstagram />
                 </a>
                 <a href="https://t.me/txtgroupuzb" className="footer_icon">
@@ -66,7 +69,7 @@ const Footer = () => {
             </div>
             <div className="footer_card3">
               <p className="footer_card3_text">
-              © 2024 TXT Group. Barcha huquqlar himoyalangan. Saytda nashr etilgan har qanday material kompaniya intellektual mulki hisoblanib, foydalanuvchilar bundan xabardor boʻlishi kerak. Ushbu saytdagi rasm va renderlar majmua loyihasi, xonadonlar joylashuvi va hovli maydoni bilan yaqindan tanishish maqsadida yuklangan. Tayyor binolar dizayni va koʻrinishi saytdagi tasvirlardan farq qilishi mumkin. Shu sababli bu maʼlumotlar ommaviy oferta hisoblanmaydi.
+                © 2024 TXT Group. Barcha huquqlar himoyalangan. Saytda nashr etilgan har qanday material kompaniya intellektual mulki hisoblanib, foydalanuvchilar bundan xabardor boʻlishi kerak. Ushbu saytdagi rasm va renderlar majmua loyihasi, xonadonlar joylashuvi va hovli maydoni bilan yaqindan tanishish maqsadida yuklangan. Tayyor binolar dizayni va koʻrinishi saytdagi tasvirlardan farq qilishi mumkin. Shu sababli bu maʼlumotlar ommaviy oferta hisoblanmaydi.
               </p>
 
             </div>
