de438e7829efe0c692daf71b359f035a3d7d5263
Behruz-developer <88540036+Behruz-developer@users.noreply.github.com>
2024-08-23 18:00:00 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,9 @@ import Houses_plan from "./Components/Houses_plan/Houses_plan";
 import { useEffect, useState } from "react";
 import Loading from "./Components/Loading/Loading";
 import average from './assets/images/avarage.jpg'
-import average2 from './assets/images/average1.jpg'
-import average3 from './assets/images/average2.jpg'
-import average4 from './assets/images/average3.jpeg'
+import average2 from './assets/images/average1.png'
+import average3 from './assets/images/average2.png'
+import average4 from './assets/images/average3.jpg'
 
 
 
